3924a52aee97a3ce55d5b473077350ce5de0c260
chuongdks <phchuogn1998@gmail.com>
2026-04-18 14:49:53 -0400

    # Commit minor minor again: update git ignore and the import method

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const apiBaseURL = process.env.REACT_APP_API_URL; // CloudFlare can change env and variable
+const apiBaseURL = import.meta.env.VITE_API_URL || 'https://devoted-meyer-stationery-fisheries.trycloudflare.com'; // CloudFlare can change env and variable
 
 export default axios.create({
     baseURL: apiBaseURL, // 172.21.176.1:8080 , https://9c96-103-106-239-104.ap.ngrok.io , 'http://<ec2-public-ip>:8080' , https://devoted-meyer-stationery-fisheries.trycloudflare.com
