71dc7c0d988d72ea758a5bafb7fe86988f83505b
chuongdks <phchuogn1998@gmail.com>
2026-04-18 13:56:58 -0400

    # Commit minor test

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+const apiBaseURL = process.env.REACT_APP_API_URL || 'https://manitoba-receptor-arrived-computing.trycloudflare.com'; // CloudFlare can change env and variable
 
 export default axios.create({
-    baseURL:'http://3.235.120.28:8080', // 172.21.176.1:8080 , https://9c96-103-106-239-104.ap.ngrok.io , 'http://<ec2-public-ip>:8080'
+    baseURL: apiBaseURL, // 172.21.176.1:8080 , https://9c96-103-106-239-104.ap.ngrok.io , 'http://<ec2-public-ip>:8080'
     headers: {"Content-Type": "application/json"} // ngrok is used to exposed the endpoint API / "Content-Type": "application/json" / "ngrok-skip-browser-warning": "true"
 })
